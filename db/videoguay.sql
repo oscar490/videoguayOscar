03bf69e20a1fb7a7fabf3757a591d0c40df47332
Óscar Vega <oscarvegaherrera59@gmail.com>
2018-02-09 20:38:05 +0100

    Se crea la tabla usuarios

--- db/videoguay.sql
+++ db/videoguay.sql
@@ -2,6 +2,17 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios
+(
+      id       BIGSERIAL PRIMARY KEY
+    , nombre   VARCHAR(255) NOT NULL UNIQUE
+    , password VARCHAR(255) NOT NULL
+    , email    VARCHAR(255)
+);
+
+CREATE INDEX idx_usurios_email ON usuarios (email);
 
 DROP TABLE IF EXISTS socios CASCADE;
 
