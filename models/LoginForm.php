655335f28283d396119103d4d8a204e6d12712e6
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-02-12 14:09:12 +0100

    Gestion de usuarios

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -47,7 +47,7 @@ class LoginForm extends Model
             $user = $this->getUser();
 
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+                $this->addError($attribute, 'Nombre de usuario o clave incorrecta.');
             }
         }
     }
